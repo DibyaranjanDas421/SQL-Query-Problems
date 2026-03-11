1a75a2fc9a32ac428609174532bae8b57b360038
DibyaranjanDas421 <dibyaranjandas468@gmail.com>
2026-03-11 22:13:03 +0530

    solved problem from sql basics

---
+++ README.md
@@ -0,0 +1,21 @@
+# SQL LeetCode Practice
+
+This repository contains my SQL solutions from LeetCode.
+
+## Day 1 - SQL Basics
+
+Problems solved:
+
+1. Recyclable and Low Fat Products (1757)
+2. Find Customer Referee (584)
+3. Big Countries (595)
+4. Article Views I (1148)
+5. Invalid Tweets (1683)
+
+Concepts:
+- SELECT
+- WHERE
+- AND / OR
+- DISTINCT
+- ORDER BY
+- String functions
---
+++ day1_basic_queries.sql
@@ -0,0 +1,31 @@
+-- Day 1 SQL Practice
+
+-- 1. Recyclable and Low Fat Products
+SELECT product_id
+FROM Products
+WHERE low_fats = 'Y' AND recyclable = 'Y';
+
+
+-- 2. Find Customer Referee
+SELECT name
+FROM Customer
+WHERE referee_id != 2 OR referee_id IS NULL;
+
+
+-- 3. Big Countries
+SELECT name, population, area
+FROM World
+WHERE area >= 3000000 OR population >= 25000000;
+
+
+-- 4. Article Views I
+SELECT DISTINCT author_id AS id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY id;
+
+
+-- 5. Invalid Tweets
+SELECT tweet_id
+FROM Tweets
+WHERE LENGTH(content) > 15;
