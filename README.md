fe9adf44839be8b1d2fce933fb1cb961ed216e3f
DibyaranjanDas421 <dibyaranjandas468@gmail.com>
2026-03-14 21:51:17 +0530

    solved some problems in conditional queries

--- README.md
+++ README.md
@@ -19,3 +19,34 @@ Concepts:
 - DISTINCT
 - ORDER BY
 - String functions
+
+
+Day 2 - Filtering Data (Conditional Queries)
+
+Problems solved:
+
+Big Countries (595)
+
+Find Customer Referee (584)
+
+Invalid Tweets (1683)
+
+Patients With a Condition (1527)
+
+Customers Who Never Order (183)
+
+Second Highest Salary (176)
+
+Concepts:
+
+WHERE clause
+
+IN / NOT IN
+
+LIKE pattern matching
+
+Subqueries
+
+ORDER BY
+
+LIMIT / OFFSET
---
+++ day2_conditional_queries.sql
@@ -0,0 +1,39 @@
+-- Problem: Big Countries
+-- Link: https://leetcode.com/problems/big-countries/
+-- Concept: WHERE, OR
+
+SELECT name, population, area
+FROM World
+WHERE area >= 3000000
+   OR population >= 25000000;
+
+
+   -- Problem: Find Customer Referee
+-- Link: https://leetcode.com/problems/find-customer-referee/
+-- Concept: WHERE, NOT
+
+SELECT name
+FROM Customer
+WHERE referee_id != 2
+   OR referee_id IS NULL;
+
+-- Problem: Patients With a Condition
+-- Link: https://leetcode.com/problems/patients-with-a-condition/
+-- Concept: LIKE
+
+SELECT *
+FROM Patients
+WHERE conditions LIKE 'DIAB1%'
+   OR conditions LIKE '% DIAB1%';
+
+-- Problem: Second Highest Salary
+-- Link: https://leetcode.com/problems/second-highest-salary/
+-- Concept: ORDER BY, LIMIT, OFFSET
+
+SELECT (
+    SELECT DISTINCT salary
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET 1
+) AS SecondHighestSalary;      
+
