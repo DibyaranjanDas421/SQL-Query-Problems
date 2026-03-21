c14be39a749afc78ced88be7e2aa5d07365aec4a
DibyaranjanDas421 <dibyaranjandas468@gmail.com>
2026-03-21 21:19:08 +0530

    solved one problem from conditional queries

--- day3_aggregation_queries.sql
+++ day3_aggregation_queries.sql
@@ -12,3 +12,24 @@ FROM (
     GROUP BY num
     HAVING COUNT(num) = 1
 ) AS single_numbers;
+
+
+-- Employees Whose Manager Left the Company | Easy
+
+SELECT employee_id
+FROM Employees e
+WHERE salary < 30000
+  AND manager_id IS NOT NULL
+  AND NOT EXISTS (
+      SELECT 1
+      FROM Employees m
+      WHERE e.manager_id = m.employee_id
+  )
+ORDER BY employee_id;select employee_id from
+
+select employee_id from
+Employees
+where salary < 30000 and manager_id not in(
+    select employee_id
+    from Employees
+)  order by employee_id asc;
