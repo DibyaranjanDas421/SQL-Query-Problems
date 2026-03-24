dd50bbd60ef43c7395de63dee50862b63704ecf1
DibyaranjanDas421 <dibyaranjandas468@gmail.com>
2026-03-24 22:09:05 +0530

    solved some sql queries from aggeregation part

--- day3_aggregation_queries.sql
+++ day3_aggregation_queries.sql
@@ -33,3 +33,32 @@ where salary < 30000 and manager_id not in(
     select employee_id
     from Employees
 )  order by employee_id asc;
+
+
+---Classes More Than 5 Students | Easy
+Select class    
+from Courses 
+group by class
+having count(*)>=5;
+
+--Biggest Single Number
+SELECT MAX(num) AS num
+FROM (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(num) = 1
+) AS single_numbers;
+
+-----Average Selling Price | Easy
+
+select p.product_id,
+ifnull(
+    round(sum(u.units*p.price)/sum(u.units),2),
+    0
+) as average_price
+from Prices p
+left join UnitsSold u
+on p.product_id=u.product_id
+and purchase_date between p.start_date and p.end_date
+group by p.product_id;
